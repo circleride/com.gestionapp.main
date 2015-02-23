34f14ca7b5bf27da55830c0d224b85b632ca2003
circleride <circleride.team@gmail.com>
2015-02-22 22:44:02 -0600

    2

--- js/index.js
+++ js/index.js
@@ -1,57 +1,53 @@
-
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('scan').addEventListener('click', this.scan, false);
-    },
+initialize: function() {
+this.bindEvents();
+},
+bindEvents: function() {
+document.addEventListener('deviceready', this.onDeviceReady, false);
+document.getElementById('scan').addEventListener('click', this.scan, false);
+},
 
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+onDeviceReady: function() {
+app.receivedEvent('deviceready');
+},
 
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+receivedEvent: function(id) {
+var parentElement = document.getElementById(id);
+var listeningElement = parentElement.querySelector('.listening');
+var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+listeningElement.setAttribute('style', 'display:none;');
+receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    },
+console.log('Received Event: ' + id);
+},
 
-    scan: function() {
-        console.log('scanning');
+scan: function() {
+console.log('scanning');
 
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.scan( function (result) {
+scanner.scan( function (result) {
 /*
 result.text
 result.format
 result.cancelled
-* */
-            //$("#info_qr").html("Cifrado: " + result.text + "\n");
-            $("#info_qr").load("https://demo.gestionapp.tk/movil/validate_qr.php?f="+result.format+"&qr=" + result.text + "");
+*/
 
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            //document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
+$("#info_qr").html("Espere un momento...");
+$("#info_qr").load("http://demo.gestionapp.tk/movil/validate_qr.php?f="+result.format+"&qr=" + result.text + "");
 
-        }, function (error) {
-            console.log("Scanning failed: ", error);
-        } );
-    }
+//console.log("Scanner result: \n" + "text: " + result.text + "\n" + "format: " + result.format + "\n" + "cancelled: " + result.cancelled + "\n");
+//document.getElementById("info").innerHTML = result.text;
+//console.log(result);
+/*
+if (args.format == "QR_CODE") {
+window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+}
+*/
+
+}, function (error) {
+//console.log("Scanning failed: ", error);
+} );
+}
 };
