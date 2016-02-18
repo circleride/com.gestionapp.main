40bbe4a3cb45efeff401b04b01ed0f487313b309
maitret <maitret@myhostmx.com>
2016-02-18 11:15:07 -0600

    :)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,136 @@
+
+
+var pushNotification;
+function onDeviceReadyPush() {
+$("#app-status-ul").append('<li>deviceready event received</li>');
+
+document.addEventListener("backbutton", function(e)
+{
+$("#app-status-ul").append('<li>backbutton event received</li>');
+
+if( $("#home").length > 0)
+{
+// call this to get a new token each time. don't call it to reuse existing token.
+//pushNotification.unregister(successHandler, errorHandler);
+e.preventDefault();
+navigator.app.exitApp();
+}
+else
+{
+navigator.app.backHistory();
+}
+}, false);
+
+try
+{
+pushNotification = window.plugins.pushNotification;
+if (device.platform == 'android' || device.platform == 'Android') {
+$("#app-status-ul").append('<li>registering android</li>');
+pushNotification.register(successHandler, errorHandler, {"senderID":"725227860513","ecb":"onNotificationGCM"});     // required!
+} else {
+$("#app-status-ul").append('<li>registering iOS</li>');
+pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
+}
+}
+catch(err)
+{
+txt="There was an error on this page.\n\n";
+txt+="Error description: " + err.message + "\n\n";
+//alert(txt);
+}
+}
+
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+if (e.alert) {
+$("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+//navigator.notification.alert(e.alert);
+}
+
+if (e.sound) {
+var snd = new Media(e.sound);
+snd.play();
+}
+
+if (e.badge) {
+pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+}
+}
+
+// handle GCM notifications for Android
+function onNotificationGCM(e) {
+$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+switch( e.event )
+{
+case 'registered':
+if ( e.regid.length > 0 )
+{
+$("#app-status-ul").append('<li>REGISTERED -> REGID:<a href="mailto:circleride.team@gmail.com?body=' + e.regid + '">' + e.regid + '</a></li>');
+window.localStorage.setItem("token_push", e.regid);
+// Your GCM push server needs to know the regID before it can push to this device
+// here is where you might want to send it the regID for later use.
+console.log("regID = " + e.regid);
+}
+break;
+
+case 'message':
+// if this flag is set, this notification happened while we were in the foreground.
+// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+if (e.foreground)
+{
+$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+// if the notification contains a soundname, play it.
+var my_media = new Media(e.soundname);
+my_media.play();
+}
+else
+{   // otherwise we were launched because the user touched a notification in the notification tray.
+if (e.coldstart)
+$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+else
+$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+}
+
+$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+break;
+
+case 'error':
+$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+break;
+
+default:
+$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+break;
+}
+}
+
+function tokenHandler (result) {
+$("#app-status-ul").append('<li>token: <a href="mailto:circleride.team@gmail.com?body=' + result + '">'+ result +'</a></li>');
+window.localStorage.setItem("token_push", result);
+// Your iOS push server needs to know the token before it can push to this device
+// here is where you might want to send it the token for later use.
+}
+
+function successHandler (result) {
+$("#app-status-ul").append('<li>success:'+ result +'</li>');
+}
+
+function errorHandler (error) {
+$("#app-status-ul").append('<li>error:'+ error +'</li>');
+}
+document.addEventListener('deviceready', onDeviceReadyPush, true);
+
+
+
+
 var app = {
 initialize: function() {
 this.bindEvents();
 },
+
 bindEvents: function() {
 document.addEventListener('deviceready', this.onDeviceReady, false);
 document.getElementById('scan').addEventListener('click', this.scan, false);
@@ -9,6 +138,15 @@ document.getElementById('scan').addEventListener('click', this.scan, false);
 
 onDeviceReady: function() {
 app.receivedEvent('deviceready');
+        $("#btnDefaultSMS").click(function(){
+            alert("click");
+            var number = $("#numberTxt").val();
+            var message = $("#messageTxt").val();
+            var intent = "INTENT"; //leave empty for sending sms using default intent
+            var success = function () { alert('Mensaje Enviado.'); };
+            var error = function (e) { alert('Mensaje Fallido: ' + e); };
+            sms.send(number, message, intent, success, error);
+        });
 },
 
 receivedEvent: function(id) {
@@ -18,36 +156,27 @@ var receivedElement = parentElement.querySelector('.received');
 
 listeningElement.setAttribute('style', 'display:none;');
 receivedElement.setAttribute('style', 'display:block;');
-
 console.log('Received Event: ' + id);
+
 },
 
 scan: function() {
-console.log('scanning');
 
 var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
 scanner.scan( function (result) {
-/*
-result.text
-result.format
-result.cancelled
-*/
 
 $("#info_qr").html("Espere un momento...");
-$("#info_qr").load("http://demo.gestionapp.tk/movil/validate_qr.php?f="+result.format+"&qr=" + result.text + "");
+$("#info_qr").load("http://app.sanzon.tk/movil/validate_qr.php?f="+result.format+"&qr=" + result.text + "&token_push=" + window.localStorage.getItem("token_push") + "");
 
-//console.log("Scanner result: \n" + "text: " + result.text + "\n" + "format: " + result.format + "\n" + "cancelled: " + result.cancelled + "\n");
-//document.getElementById("info").innerHTML = result.text;
-//console.log(result);
-/*
-if (args.format == "QR_CODE") {
+if (result.format == "QR_CODE") {
 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
 }
-*/
 
 }, function (error) {
-//console.log("Scanning failed: ", error);
+alert("Escaneo cancelado: " + error);
+$("#info_qr").html("Espere un momento...");
+$("#info_qr").load("http://app.sanzon.tk/movil/validate_qr.php?m="+error+"&token_push=" + window.localStorage.getItem("token_push"));
 } );
 }
 };
+
