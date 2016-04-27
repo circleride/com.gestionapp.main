7ea940ba700bb243d420d2d53a8b85d637ee5068
maitret <maitret@myhostmx.com>
2016-04-27 17:17:39 -0500

    :)

--- assets/js/index.js
+++ assets/js/index.js
@@ -104,3 +104,72 @@ setTimeout(function() {
 window.handle_url = url;
 }, 0);
 }
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('scan').addEventListener('click', this.scan, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+    },
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "725227860513"
+            },
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+        push.on('registration', function(data) {
+            $("#info_device").append(JSON.stringify(data));
+            window.localStorage.setItem("token_push", JSON.stringify(data));
+            console.log('registration event: ' + data.registrationId);
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+            $("#info_device").append(JSON.stringify(e));
+        });
+        push.on('notification', function(data) {
+            $("#info_device").append(JSON.stringify(data));
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+       });
+}
+
+};
