34d624771cb016b54c8b912ada71b78375be4bc0
maitret <maitret@myhostmx.com>
2016-04-28 02:03:52 -0500

    :)

--- assets/js/index.js
+++ assets/js/index.js
@@ -143,14 +143,15 @@ var app = {
         console.log('after init');
         push.on('registration', function(data) {
             //$("#info_device").append(JSON.stringify(data));
-            window.localStorage.setItem("token_push", JSON.stringify(data));
             console.log('registration event: ' + data.registrationId);
             var oldRegId = localStorage.getItem('registrationId');
             if (oldRegId !== data.registrationId) {
                 // Save new registration ID
                 localStorage.setItem('registrationId', data.registrationId);
                 // Post registrationId to your app server as the value has changed
+                window.localStorage.setItem("token_push", JSON.stringify(data));
             }
+
             var parentElement = document.getElementById('registration');
             var listeningElement = parentElement.querySelector('.waiting');
             var receivedElement = parentElement.querySelector('.received');
